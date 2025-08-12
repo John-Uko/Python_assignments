45cd215669009e92f1810da357e911a5d49e78bf
John-Uko <ukojohn.j@gmail.com>
2025-08-12 11:56:37 +0100

    week2

---
+++ week2.py
@@ -0,0 +1,21 @@
+my_list = []
+print(my_list)
+my_list.append(10)
+print(my_list)
+my_list.append(20)
+print(my_list)
+my_list.append(30)
+print(my_list)
+my_list.append(40)
+print(my_list)
+my_list.insert(1,15)
+print(my_list)
+list_2 = [50, 60, 70]
+print(list_2)
+my_list.extend(list_2)
+print(my_list)
+my_list.pop(-1)
+print(my_list)
+my_list.sort
+print(my_list)
+print(my_list.index(30))
